fd14549f457b02a7f1caf235b84cd39c38b65ea2
Supriya9090 <supriyapatole0123@gmail.com>
2020-10-23 20:54:26 -0700

    Take user input from the kyeboard

---
+++ helloworldv1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  char name[20];
+  printf("Enter your frist name:\n");
+  scanf("%s",name);
+  printf("your name is %s,welcome to github world\n",name);
+  return 0;
+
+
+}
